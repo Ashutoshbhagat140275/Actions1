ccfdbee7655a26531d91a7e22a86a6e682ba29d1
Ashutoshbhagat140275 <149521448+Ashutoshbhagat140275@users.noreply.github.com>
2025-05-17 18:10:43 +0530

    Update test_operations.py

--- test/test_operations.py
+++ test/test_operations.py
@@ -2,13 +2,13 @@ from src.app import add,sub,mul,div
 
 def test_add():
     assert add(-1,2)==1
-    assert add(0,0)==1
+    assert add(0,0)==0
     assert add(9,10)==19
 
 def test_sub():
     assert sub(-1,1)==-2
     assert sub(1,1)==0
-    assert sub(10,12)==22
+    assert sub(10,12)==-2
 
 def test_mul():
     assert mul(1,0)==0
@@ -18,6 +18,6 @@ def test_mul():
 def test_div():
     assert div(2,4)==0.5
     assert div(6,3)==2
-    assert div(4,10)==2.5
+    assert div(4,10)==0.4
     with pytest.raises(ValueError):
         div(1,0)
